7af88c0742ad78da0f52cd52d14e24ce462a23bd
Josesmar <josesmargds@gmail.com>
2021-04-20 22:08:38 -0300

    Validador de senhas com requisitos

--- ValidadorSenhaComRequisitos/src/main/java/ValidaSenhaComRequisitos.java
+++ ValidadorSenhaComRequisitos/src/main/java/ValidaSenhaComRequisitos.java
@@ -1,36 +1,21 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class ValidaSenhaComRequisitos {
-    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws IOException {
-        String nome = in.readLine();
-        try {
-            FileReader arq = new FileReader(nome);
-            BufferedReader lerArq = new BufferedReader(arq);
+    public static void main(String[] args) {
+        Scanner lerDados = new Scanner(System.in);
+        String senha;
+        final String requeridosNecessarios = "^(?:(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).*)$";
+        final String requisitosPermitidos = "[a-zA-Z0-9]+";
 
-            String linha = lerArq.readLine(); // lê a primeira linha
+        while(lerDados.hasNextLine()) {
+            senha = lerDados.nextLine();
 
-            while (linha != null) {
-                validadorDeSenha(linha);
-                linha = lerArq.readLine(); // lê da segunda até a última linha
+            if ((senha.length() >=6) && (senha.length() <= 32) &&
+              (senha.matches(requisitosPermitidos)) && (senha.matches(requeridosNecessarios))){
+                System.out.println("Senha valida.");
+            } else {
+                System.out.println("Senha invalida.");
             }
-            arq.close();
-        } catch (IOException e) {
-        }
-    }
-    public static boolean senhaValida(String Senha){
-        return "\"^(?=.*[az])(?=.*[AZ])(?=.*\\d)[a-zA-Z\\d]{6,32}$\"".matches(Senha);
-    }
-    public static void validadorDeSenha(String senha) {
-        if ((senha.contains("'!@#$%¨&*()")) || (senha.length() < 6) || (senha.length() > 32) || (senha.length() == 0)
-           && (!senhaValida(senha))) {
-            System.out.println("Senha invalida.");
-        } else {
-            System.out.println("Senha valida.");
         }
     }
 }
