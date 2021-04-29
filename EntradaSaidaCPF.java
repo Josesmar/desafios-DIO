b76243368e7064392bc6f13cdd5379fdb389d8a5
Josesmar <josesmargds@gmail.com>
2021-04-28 21:35:55 -0300

    EntradaSaidaCPF

---
+++ EntradaSaidaCPF.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class EntradaSaidaCPF {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(new Locale("en", "US"));
+        Scanner sc = new Scanner(System.in);
+
+        String[] cpf = new String[4];
+        cpf = sc.nextLine().split("\\.|-");
+
+        for(int i = 0 ;  i < 4  ; i++)
+            System.out.printf("%s\n",cpf[i]);
+        sc.close();
+    }
+}
