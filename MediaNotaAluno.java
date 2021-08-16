8a63e8b8d0ee73d285d2b30e3df47355651bc8d4
Josesmar <josesmargds@gmail.com>
2021-08-16 08:48:13 -0300

    media nota aluno

--- MediaNotaAluno.java
+++ MediaNotaAluno.java
@@ -47,46 +47,52 @@ import java.util.Scanner;
 
 public class MediaNotaAluno{
     public static void main(String[] args) throws IOException {
+
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
         Locale.setDefault(new Locale("en", "US"));
 
-        float n1,n2,n3,n4,media,emexame,emexamefinal;
+        float n1,n2,n3,n4,media;
+        double emexame,emexamefinal;
 
         n1 = sc.nextFloat();
         n2 = sc.nextFloat();
         n3 = sc.nextFloat();
         n4 = sc.nextFloat();
-        emexame = sc.nextFloat();
 
-        media = ((n1 + n2 + n3 + n4) / 4);
-        emexamefinal = ((media  + emexame) / 2);
-
-        System.out.printf("Media: %.1f\n",media);
+        media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / 10;
 
         //continue a solucao de acordo com o enunciado
+        System.out.printf("Media: %.1f%n" , media);
 
         if (media >= 7.0){
             System.out.printf("Aluno aprovado.\n");
         }
-        else if (media < 5.0){
+        if (media < 5.0) {
             System.out.printf("Aluno reprovado.\n");
         }
-        else {
+        if (media >= 5.0 && media <= 6.9) {
             System.out.printf("Aluno em exame.\n");
-
+            emexame = sc.nextDouble();
             System.out.printf("Nota do exame: %.1f\n", emexame );
+            emexamefinal = ((emexame + media) / 2.0);
 
-            if(media >= 7.0){
+            if (emexamefinal >= 5.0) {
                 System.out.printf("Aluno aprovado.\n");
                 System.out.printf("Media final: %.1f\n", emexamefinal );
-            }
-            else{
+            } else{
                 System.out.printf("Aluno reprovado.\n");
+
+
+
                 System.out.printf("Media final: %.1f\n", emexamefinal );
+
             }
+
         }
 
         sc.close();
     }
 }
+
+ 
